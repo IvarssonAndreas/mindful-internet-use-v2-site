05914e1fc98db35f5fc3c19aaa230d40dd6536a0
Andreas Ivarsson <ivarsson.andreas@hotmail.se>
2021-10-09 15:26:06 +0200

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material'
 import theme from '../theme'
 import { globalStyles } from '../global-styles'
@@ -11,6 +12,33 @@ import '@fontsource/comfortaa/700.css'
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <GlobalStyles styles={globalStyles} />
